dbd84339b3a36cde8d5fca8893fa83a695fe5e5e
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2022-11-10 10:03:47 +0600

    updated debounce & throttle hook call

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -5,18 +5,14 @@ import useThrottle from "../../hooks/useThrottle";
 const Search = () => {
   const dispatch = useAppDispatch();
 
-  const fetchVideos = useThrottle((inputText: string) => {
+  const handleChange = useThrottle((inputText: string) => {
     dispatch(search(inputText));
     console.log("Fetching : ", inputText);
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    fetchVideos(e.target.value);
-  };
-
   return (
     <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
-      <input className="outline-none border-none mr-2" type="search" name="search" placeholder="Search" onChange={handleChange} />
+      <input className="outline-none border-none mr-2" type="search" name="search" placeholder="Search" onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e.target.value)} />
       <i className=" fa-solid fa-magnifying-glass cursor-pointer text-yellow-400"></i>
     </div>
   );
